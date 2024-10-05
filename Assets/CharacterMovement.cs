9b322ed3f4fd1bf10e7170f91870d9531d3f90e6
ben9dot9 <132110521+ben9dot9@users.noreply.github.com>
2024-10-05 12:10:23 +0200

    Player: 1ere version du CharacterController
    Inputs au clavier pour l'instant

---
+++ Assets/CharacterMovement.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    public CharacterController controller;
+    public float playerSpeed = 1.0f;
+    private Vector3 playerVelocity;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        controller.Move(move * Time.deltaTime * playerSpeed);
+
+        if (move != Vector3.zero)
+        {
+            gameObject.transform.forward = move;
+        }
+    }
+}
