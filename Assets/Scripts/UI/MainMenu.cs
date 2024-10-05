d6fc9c86011a5de4966640475607066b6d9c4b67
Erwan O <81764792+SunsetMemory@users.noreply.github.com>
2024-10-05 19:48:55 +0200

    Update MainMenu

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -1,11 +1,12 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class NewMonoBehaviourScript : MonoBehaviour
+public class MainMenu : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public void StartGame()
     {
-        
+        SceneManager.LoadSceneAsync("SampleScene");
     }
 
     // Update is called once per frame
