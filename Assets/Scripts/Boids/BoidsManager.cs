cf54c423d8959cd9a87bdc459d5acbb3ada0108a
FloreauLuca <lucafloreau@akeonet.com>
2024-10-05 18:43:12 +0200

    Add HUD functions

--- Assets/Scripts/Boids/BoidsManager.cs
+++ Assets/Scripts/Boids/BoidsManager.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class BoidsManager : MonoBehaviour
 {
+	public static BoidsManager Instance = null;
+
 	[SerializeField] private int _count = 100;
     private List<Boids> _boids = new List<Boids>();
 	[SerializeField] private List<Transform> _fleePoints = new List<Transform>();
 	[SerializeField] private Transform _player = null;
 	[SerializeField] private List<BoidsParameters> _boidsParameters = null;
 
+	private void Awake()
+	{
+		Instance = this;
+	}
+
 	private void Start()
 	{
-        for (int i = 0; i < _count; i++)
+
+		for (int i = 0; i < _count; i++)
         {
 			BoidsParameters boidParameters = _boidsParameters[Random.Range(0, _boidsParameters.Count)];
 			Boids boid = Instantiate(boidParameters.BoidPrefab, transform);
@@ -28,4 +36,30 @@ public class BoidsManager : MonoBehaviour
 			boid.UpdateBoids(_boids, _fleePoints, _player);
 		}
     }
+
+	public int GetBoidsCount()
+	{
+		Debug.Log(_boids.Count);
+		return _boids.Count;
+	}
+
+	public void Lure()
+	{
+		Debug.Log("Lure");
+	}
+
+	public void Cry()
+	{
+		Debug.Log("Cry");
+	}
+
+	public void Move()
+	{
+		Debug.Log("Move");
+	}
+
+	public void SpawnFlee()
+	{
+		Debug.Log("SpawnFlee");
+	}
 }
